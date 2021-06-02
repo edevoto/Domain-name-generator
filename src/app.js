7f97f1c44159f99b91dfcde91e3bce944f242590
enzo devoto <enzodevotob@gmail.com>
2021-06-02 15:01:17 -0400

    Finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let hacks = [".com", ".net", ".cl", ".org"];
+
+let randomNumber = max => Math.floor(Math.random() * max);
+
+const getRandom = (pron, adj, noun, hacks) => {
+  return `${pron[randomNumber(pron.length)]}${adj[randomNumber(adj.length)]}${
+    noun[randomNumber(noun.length)]
+  }${hacks[randomNumber(hacks.length)]}`;
 };
+let randomName = document.querySelector("#randomName");
+
+const showRandom = () => {
+  randomName.innerHTML = getRandom(pronoun, adj, noun, hacks);
+};
+
+const boton = document
+  .querySelector("#hitMe")
+  .addEventListener("click", showRandom);
